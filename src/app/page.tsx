78f6a16f98667b95cba81d08b2e5e4957298b828
iyin0 <p2iyinoluwa@gmail.com>
2024-03-02 13:52:13 +0100

    first look without header and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,265 @@
+'use client'
+
 import Image from "next/image";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import logo from '@/assets/PlaceholderCompanyLogos.png'
+import { Textarea } from "@/components/ui/textarea"
+
+const formSchema = z.object({
+  companyName: z.string().min(2).max(50),
+  companyWebsite: z.string().min(2).max(50),
+  companyLinkedIn: z.string().min(2).max(50),
+  companyIndustry: z.string().min(2).max(50),
+  employeeCount: z.string(),
+  companyDescription: z.string().min(2).max(1000),
+  companyGoals: z.string().min(2).max(100),
+  companyHeadquarters: z.string().min(2).max(50),
+  fundingRound: z.string(),
+  faq: z.string().min(2).max(50),
+})
+
 
 export default function Home() {
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      companyName: "",
+      companyWebsite: "",
+      companyLinkedIn: "",
+      companyIndustry: "",
+      employeeCount: "",
+      companyDescription: "",
+      companyGoals: "",
+      companyHeadquarters: "",
+      fundingRound: "",
+      faq: "",
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+
+    console.log(values)
+  }
+
+  const counts = ['1-10', '10-100', '100-500', '1000+']
+  const stages = ['Seed', 'Angel', 'Venture', 'Growth']
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main className="w-full flex flex-col items-center pt-12 px-6 pb-6 md:py-12">
+      <div className="w-full md:max-w-[1206px]">
+        <h1 className="text-[28px] font-bold mb-[32px] md:text-[38px]">Settings</h1>
+        <Tabs defaultValue="company" className="w-[100%]">
+          <TabsList className="w-[100%] flex justify-start overflow-x-scroll bg-[#FAFAFA] p-[6px] no-scrollbar rounded-xl mb-[32px] md:w-fit">
+            <TabsTrigger value="profile" className="text-[14px] font-medium">Your Profile</TabsTrigger>
+            <TabsTrigger value="company" className="text-[14px] font-medium">Company Info</TabsTrigger>
+            <TabsTrigger value="seats" className="text-[14px] font-medium">Manage Seats</TabsTrigger>
+            <TabsTrigger value="contact" className="text-[14px] font-medium">Do not contact</TabsTrigger>
+            <TabsTrigger value="integrations" className="text-[14px] font-medium">Integrations</TabsTrigger>
+          </TabsList>
+          <TabsContent value="profile"></TabsContent>
+          <TabsContent value="company">
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                <div className="w-full flex flex-col mb-12 md:flex-row md:justify-between">
+                  <div className="flex w-full justify-between flex-wrap mb-[30px] md:order-1 md:mb-0 md:w-[auto] md:flex-row md:flex-nowrap md:space-x-[12px]">
+                    <Button className="w-full font-medium max-w-[165px] h-[38px] bg-white text-black rounded-2 text-[14px] border-[1px] border-[#17171F1A] shadow-md hover:bg-white md:w-[67px]">Cancel</Button>
+                    <Button type="submit" className="w-full font-medium max-w-[165px] h-[38px] rounded-2 bg-[#17171F] text-[14px] hover:bg-[#17171F] md:w-[110px]">Submit</Button>
+                  </div>
+                  <div className="flex justify-between items-center md:space-x-[30px]">
+                    <Image src={logo} alt="Company logo" width={108} height={108} />
+                    <div>
+                      <div className="flex items-center text-[12px] space-x-[12px] mb-[12px]">
+                        <Button className="w-[64px] h-[32px] font-medium rounded-[6px] bg-[#CA3A31] text-white text-[12px] px-[10px] py-[6px] hover:bg-[#CA3A31]">Remove</Button>
+                        <Button className="w-[85px] h-[32px] font-medium rounded-[6px] bg-white text-black text-[12px] px-[10px] py-[6px] border-[1px] border-[#17171F1A] shadow-md hover:bg-white">Change Photo</Button>
+                      </div>
+                      <p className="text-[#17171F] text-opacity-40 font-medium text-[14px]">or drag and drop (SVG, PNG, JPG)</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="md:flex md:flex-row md:flex-wrap md:justify-between">
+                  <FormField
+                    control={form.control}
+                    name="companyName"
+                    render={({ field }) => (
+                      <FormItem className="mb-[28px] md:w-full md:max-w-[562px]">
+                        <FormLabel className="font-medium text-sm mb-2">Company&apos;s Name</FormLabel>
+                        <FormControl>
+                          <Input placeholder="Sixteen Inc." {...field} className="font-medium text-sm text-[#17171F] mb-7" />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="companyWebsite"
+                    render={({ field }) => (
+                      <FormItem className="mb-[28px] md:w-full md:max-w-[562px]">
+                        <FormLabel className="font-medium text-sm mb-2">Company&apos;s Website</FormLabel>
+                        <FormControl>
+                          <Input placeholder="https://sixteen.life/" {...field} className="font-medium text-sm text-[#17171F] mb-7" />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="companyLinkedIn"
+                    render={({ field }) => (
+                      <FormItem className="mb-[28px] md:w-full md:max-w-[562px]">
+                        <FormLabel className="font-medium text-sm mb-2">Company&apos;s LinkedIn</FormLabel>
+                        <FormControl>
+                          <Input placeholder="https://www.linkedin.com/company/sixteenlife" {...field} className="font-medium text-sm text-[#17171F] mb-7" />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="companyIndustry"
+                    render={({ field }) => (
+                      <FormItem className="mb-[28px] md:w-full md:max-w-[562px]">
+                        <FormLabel className="font-medium text-sm mb-2">Company&apos;s Industry</FormLabel>
+                        <FormControl>
+                          <Input placeholder="Digital Wellbeing" {...field} className="font-medium text-sm text-[#17171F] mb-7" />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <FormField
+                  control={form.control}
+                  name="employeeCount"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="font-medium text-sm mb-2">Employee Count</FormLabel>
+                      <FormControl>
+                        <RadioGroup
+                          onValueChange={field.onChange}
+                          defaultValue={field.value}
+                          className="font-medium text-sm mb-7 flex space-x-3 flex-wrap items-center"
+                        >
+                          {counts.map((item, index) => (
+                            <div className="flex items-center space-x-2" key={index}>
+                              <Label htmlFor={item} className="h-[38px] w-fit px-3 py-2 rounded-[100px] cursor-pointer font-medium text-sm text-[#17171F] border-[1px] border-[#17171F1A] has-[:checked]:bg-black has-[:checked]:text-white">
+                                <RadioGroupItem value={item} id={item} className="hidden" />
+                                {item}
+                              </Label>
+                            </div>
+                          ))}
+                        </RadioGroup>
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="companyDescription"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="font-medium text-sm mb-2">Company&apos;s Description</FormLabel>
+                      <FormControl>
+                        <Textarea placeholder="Redesign your digital life, reduce your screen time" {...field} className="min-h-9 h-[38px] font-medium text-sm text-[#17171F] mb-7 resize-none md:h-[38px]" />
+                      </FormControl>
+                      <FormDescription>
+                        Your detailed company description
+                      </FormDescription>
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="companyGoals"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="font-medium text-sm mb-2">What are your company goals?</FormLabel>
+                      <FormControl>
+                        <Textarea placeholder="Help everyone to be more conscious of where they are spending their time" {...field} className="h-[60px] min-h-9 resize-none md:h-[38px]" />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="companyHeadquarters"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="font-medium text-sm mb-2">Company&apos;s Headquarters</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Delhi, India" {...field} className="font-medium text-sm text-[#17171F] mb-7" />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+                <div className="flex flex-col md:flex-row md:flex-wrap md:justify-between">
+                  <FormField
+                    control={form.control}
+                    name="fundingRound"
+                    render={({ field }) => (
+                      <FormItem className="mb-[28px] md:w-full md:max-w-[562px]">
+                        <FormLabel className="font-medium text-sm mb-2">Funding Round</FormLabel>
+                        <FormControl>
+                          <Select
+                            onValueChange={field.onChange}
+                            defaultValue={field.value}
+                          >
+                            <SelectTrigger className="w-[100%] font-medium text-sm text-[#17171F]">
+                              <SelectValue placeholder="Seed" />
+                            </SelectTrigger>
+                            <SelectContent>
+                              {stages.map((item, index) => (
+                                <SelectItem value={item} key={index}>{item}</SelectItem>
+                              ))}
+                            </SelectContent>
+                          </Select>
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="faq"
+                    render={({ field }) => (
+                      <FormItem className="md:w-full md:max-w-[562px]">
+                        <FormLabel className="font-medium text-sm mb-2">FAQs</FormLabel>
+                        <FormControl>
+                          <Input placeholder="https://sixteen.life/faq" {...field} className="font-medium text-sm text-[#17171F] mb-7" />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                </div>
+              </form>
+            </Form>
+          </TabsContent>
+          <TabsContent value="seats"></TabsContent>
+          <TabsContent value="contact"></TabsContent>
+          <TabsContent value="integrations"></TabsContent>
+        </Tabs>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   );
